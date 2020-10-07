dda0080e4ee910a844eb2f6474edfd034227d761
Mohamed Kamil Bin Mansor <kamilnizam@gmail.com>
2020-10-07 12:50:19 +0800

    Updates

---
+++ precache-manifest.db18eb3735b54c7757d2fdf3ff761303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb5e37f57a793e12941d8b9bad55833",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "812bb4add02b5d92086f",
+    "url": "/react-weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "445608fd80bb95a94a3d",
+    "url": "/react-weather/static/js/2.e17f5edd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.e17f5edd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812bb4add02b5d92086f",
+    "url": "/react-weather/static/js/main.c70adf2f.chunk.js"
+  },
+  {
+    "revision": "f638013bd88fa5b26bac",
+    "url": "/react-weather/static/js/runtime-main.b9e6359b.js"
+  }
+]);
